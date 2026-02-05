526dbd3e1f1bd1e5b7ec04f3dedabe389d8afb24
baticlean <baticlean225@gmail.com>
2026-02-05 23:13:43 +0000

    Correction

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.8p9gehlbmm"
+    "revision": "0.2mgbraht8qo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
